cc6ab68e55d3d6d8d3a000590cc2f2a8afe58170
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-11-16 05:40:02 +0700

    W12: Soal 12

--- week-12/src/books/lib/geolocation.dart
+++ week-12/src/books/lib/geolocation.dart
@@ -25,18 +25,23 @@ class _LocationScreenState extends State<LocationScreen> {
   Future<Position> getPosition() async {
     await Geolocator.requestPermission();
     await Geolocator.isLocationServiceEnabled();
+    await Future.delayed(const Duration(seconds: 3));
     Position? position = await Geolocator.getCurrentPosition();
     return position;
   }
 
   @override
   Widget build(BuildContext context) {
+    final myWidget =
+        myPosition == '' ? const CircularProgressIndicator() : Text(myPosition);
+    ;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Current Location Laila"),
       ),
       body: Center(
-        child: Text(myPosition),
+        child: (myWidget),
       ),
     );
   }
