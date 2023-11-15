5e1e4549868cf8ba6b3c8e416b17d4ad34a07dbb
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-11-15 10:45:34 +0700

    W12: Soal 11

---
+++ week-12/src/books/lib/geolocation.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LocationScreen extends StatefulWidget {
+  const LocationScreen({super.key});
+
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  String myPosition = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getPosition().then((Position myPos) {
+      setState(() {
+        myPosition =
+            'Latitude: ${myPos.latitude.toString()} - Longitude: ${myPos.longitude.toString()}';
+      });
+    });
+  }
+
+  Future<Position> getPosition() async {
+    await Geolocator.requestPermission();
+    await Geolocator.isLocationServiceEnabled();
+    Position? position = await Geolocator.getCurrentPosition();
+    return position;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Current Location Laila"),
+      ),
+      body: Center(
+        child: Text(myPosition),
+      ),
+    );
+  }
+}
