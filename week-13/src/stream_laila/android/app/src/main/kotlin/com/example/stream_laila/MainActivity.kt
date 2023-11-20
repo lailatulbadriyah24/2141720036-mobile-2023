dac3ee6b2917bad4e6eee9b94b847cfe15b58d55
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-11-20 09:33:19 +0700

    W13: Jawaban Soal 1

---
+++ week-13/src/stream_laila/android/app/src/main/kotlin/com/example/stream_laila/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stream_laila
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ week-13/src/stream_laila/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Stream Laila',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: const Color.fromARGB(255, 164, 214, 255))),
+        home: const StreamHomePage());
+  }
+}
+
+class StreamHomePage extends StatefulWidget {
+  const StreamHomePage({super.key});
+
+  @override
+  State<StreamHomePage> createState() => _StreamHomePageState();
+}
+
+class _StreamHomePageState extends State<StreamHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
